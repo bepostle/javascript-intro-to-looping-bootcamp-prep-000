5d7d92889908a94fe48cca989913d108231a7fe9
Brent Postlethweight <brentpostle@gmail.com>
2018-09-05 03:13:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,9 +2,12 @@ array = [];
 
 for (let i = 0; i < 25; i++) {
   if i = 1 {
-    array.push()
-  }
+    
+    array.push("I am 1 strange loop.")
+    
+  } else {
   
   array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`))
+}
 }
 
