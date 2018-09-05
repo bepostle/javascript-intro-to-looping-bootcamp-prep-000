f7fa09b80776d2d4e69d4517e9e4d142879c9e7a
Brent Postlethweight <brentpostle@gmail.com>
2018-09-05 03:22:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forLoop(array) {
 
-for (let i = 0; i < 25; i++) {
-  if (i === 1) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      
+      array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+      
+    } else {
     
-    array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
-    
-//  } else {
-  
-//  array.push(`"I am ${i} strange loops."`);
+      array.push(`"I am ${i} strange loops."`);
 }
 }
